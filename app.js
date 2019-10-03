b8e5cff50ebba9d50240d7f359f5d553df3d65f0
CN72 <cn72@CN72.local>
2019-10-03 10:14:52 +0100

    added media queries for styling and stored mole images as variables in javascript file

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+
+const mole1 = document.getElementById('mole-1')
+const mole2 = document.getElementById('mole-2')
+const mole3 = document.getElementById('mole-3')
+const mole4 = document.getElementById('mole-4')
+const mole5 = document.getElementById('mole-5')
+const mole6 = document.getElementById('mole-6')
+const moles = [mole1, mole2, mole3, mole4, mole5, mole6]
+
+let random = Math.round(Math.random()*5)
+
+
